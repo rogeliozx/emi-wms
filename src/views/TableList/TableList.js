56fb987e1e39c85494f8dda8cd3c829b3a3a2906
rogelioltz <rogelioltz@gmail.com>
2019-11-28 08:06:06 -0700

    feat: check table

--- src/views/TableList/TableList.js
+++ src/views/TableList/TableList.js
@@ -1,22 +1,19 @@
-import React,{useMemo} from 'react';
-import DataTable from 'react-data-table-component';
-import Button from '@material-ui/core/Button';
+import React, { useMemo } from "react";
+import DataTable from "react-data-table-component";
+import Button from "@material-ui/core/Button";
 
-function convertArrayOfObjectsToCSV(array){
+function convertArrayOfObjectsToCSV(array) {
   let result;
-
-  const columnDelimiter = ',';
-  const lineDelimiter = '\n';
+  const columnDelimiter = ",";
+  const lineDelimiter = "\n";
   const keys = Object.keys(array[0]);
-
-  result = '';
+  result = "";
   result += keys.join(columnDelimiter);
   result += lineDelimiter;
-
   array.forEach(item => {
     let ctr = 0;
     keys.forEach(key => {
-      if(ctr > 0) result += columnDelimiter;
+      if (ctr > 0) result += columnDelimiter;
 
       result += item[key];
 
@@ -24,29 +21,37 @@ function convertArrayOfObjectsToCSV(array){
     });
     result += lineDelimiter;
   });
-
   return result;
 }
 
-function downloadCSV(array){
-  const link = document.createElement('a');
+function downloadCSV(array) {
+  const link = document.createElement("a");
   let csv = convertArrayOfObjectsToCSV(array);
-  if(csv == null) return;
-  const filename = 'export.csv';
-  if(!csv.match(/^data:text\/csv/i)) {
+  if (csv == null) return;
+  const filename = "export.csv";
+  if (!csv.match(/^data:text\/csv/i)) {
     csv = `data:text/csv;charset=utf-8,${csv}`;
   }
-  link.setAttribute('href', encodeURI(csv));
-  link.setAttribute('download', filename);
+  link.setAttribute("href", encodeURI(csv));
+  link.setAttribute("download", filename);
   link.click();
 }
 
-const Export = ({onExport}) => (
-  <Button variant="contained"  color="primary"  onClick={e => onExport(e.target.value)}>Export</Button>
+const Export = ({ onExport }) => (
+  <Button
+    variant="contained"
+    color="primary"
+    onClick={e => onExport(e.target.value)}
+  >
+    Export
+  </Button>
 );
 
-const TableList = ({data, columns, title}) => {
-  const actionsMemo = useMemo(() => <Export onExport={() => downloadCSV(data)}/>, []);
+const TableList = ({ data, columns, title }) => {
+  const actionsMemo = useMemo(
+    () => <Export onExport={() => downloadCSV(data)} />,
+    []
+  );
 
   return (
     <DataTable
@@ -54,6 +59,7 @@ const TableList = ({data, columns, title}) => {
       columns={columns}
       data={data}
       actions={actionsMemo}
+      selectableRows
     />
   );
 };
